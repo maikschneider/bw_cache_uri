66d25a57c8c9ba55822c08a3b9e0ae2d2d402325
Maik Schneider <m.schneider@blueways.de>
2019-10-05 18:24:39 +0200

    feat: add parser task

--- Classes/Task/DomParserTask.php
+++ Classes/Task/DomParserTask.php
@@ -2,6 +2,9 @@
 
 namespace Blueways\BwCacheUri\Task;
 
+use Blueways\BwCacheUri\Utility\DomLoaderUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Scheduler\Task\AbstractTask;
 
 class DomParserTask extends AbstractTask
@@ -9,6 +12,36 @@ class DomParserTask extends AbstractTask
 
     public function execute()
     {
-        // TODO: Implement execute() method.
+        /** @var \TYPO3\CMS\Core\Database\Query\QueryBuilder $queryBuilder */
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+
+        /** @var DomLoaderUtility $domLoader */
+        $domLoader = GeneralUtility::makeInstance(DomLoaderUtility::class);
+
+        // find all html elements with parse uri
+        $statement = $queryBuilder
+            ->select('uid', 'dom_uri', 'dom_filter', 'dom_processor', 'bodytext')
+            ->from('tt_content')
+            ->where(
+                $queryBuilder->expr()->neq('dom_uri', $queryBuilder->createNamedParameter(''))
+            )
+            ->execute();
+
+        while ($row = $statement->fetch()) {
+
+            $domLoader->setFilter($row['dom_filter']);
+            $domLoader->setProcessor($row['dom_processor']);
+            $html = $domLoader->getDomFromUri($row['dom_uri']);
+
+            $queryBuilder
+                ->update('tt_content')
+                ->set('bodytext', $html)
+                ->where(
+                    $queryBuilder->expr()->eq('uid', $row['uid'])
+                )
+                ->execute();
+        }
+
+        return true;
     }
 }
