74ada7ff172a5e0b25002f794ee5b8444221e73c
Maik Schneider <m.schneider@blueways.de>
2019-10-05 04:11:57 +0200

    feat: dom parser, tce hook to crawl before save

--- Classes/Hooks/TCEmainHook.php
+++ Classes/Hooks/TCEmainHook.php
@@ -2,6 +2,10 @@
 
 namespace Blueways\BwCacheUri\Hooks;
 
+use Blueways\BwCacheUri\Utility\DomLoaderUtility;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class TCEmainHook
 {
 
@@ -11,9 +15,26 @@ class TCEmainHook
         $id,
         $dataHandler
     ) {
-        // @TODO check of new entry (no $id)
         if ($table === 'tt_content' && $id && $dataHandler->datamap['tt_content'][$id]['CType'] === 'html' && $dataHandler->datamap['tt_content'][$id]['dom_uri'] !== "") {
-            // @TODO start parser, save dom
+            $uri = $dataHandler->datamap['tt_content'][$id]['dom_uri'];
+
+            $domLoader = GeneralUtility::makeInstance(DomLoaderUtility::class);
+            $html = $domLoader->getDomFromUri($uri);
+
+            if ($html !== '') {
+
+                $fieldArray['bodytext'] = $html;
+
+                $flashMessageService = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\FlashMessageService::class);
+                $message = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(FlashMessage::class,
+                    'LLL:EXT:bw_cache_uri/Resources/Private/Language/locallang.xlf:message.success.body',
+                    'LLL:EXT:bw_cache_uri/Resources/Private/Language/locallang.xlf:message.success.title',
+                    FlashMessage::OK,
+                    true
+                );
+                $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
+                $messageQueue->addMessage($message);
+            }
         }
     }
 }
---
+++ Classes/Utility/DomLoaderUtility.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Blueways\BwCacheUri\Utility;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class DomLoaderUtility
+{
+
+    protected $uri;
+
+    protected $dom;
+
+    protected $filter;
+
+    protected function loadDom()
+    {
+        $client = HttpClient::create();
+        try {
+            $response = $client->request('GET', $this->uri);
+            $this->dom = $response->getContent();
+        } catch (TransportExceptionInterface $e) {
+        } catch (ClientExceptionInterface $e) {
+        } catch (RedirectionExceptionInterface $e) {
+        } catch (ServerExceptionInterface $e) {
+        }
+    }
+
+    public function crawlDom()
+    {
+        if (!$this->filter) {
+            return;
+        }
+
+        $crawler = new Crawler($this->dom);
+        $crawler->filter($this->filter);
+        $this->dom = $crawler->html('');
+    }
+
+    public function getDomFromUri($uri)
+    {
+        $this->uri = $uri;
+
+        $this->loadDom();
+        $this->crawlDom();
+
+        return $this->dom;
+    }
+}
